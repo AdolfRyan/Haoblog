e4b77846b189198b55141d450e9980549933ed30
Liu Hao <adolfryan616919637@gmail.com>
2024-05-21 20:20:15 +0800

    webiste注释

--- packages/website/components/AuthorCard/index.tsx
+++ packages/website/components/AuthorCard/index.tsx
@@ -1,3 +1,4 @@
+//作者卡片
 import Link from "next/link";
 import { useContext, useEffect, useMemo } from "react";
 import Headroom from "headroom.js";
@@ -18,7 +19,41 @@ export interface AuthorCardProps {
   showSubMenu: "true" | "false";
   showRSS: "true" | "false";
 }
-
+// sticky：这个类使元素的定位方式为 sticky，也就是当页面滚动到一定位置时，元素会固定在屏幕上。
+// w-52：这个类设置元素的宽度为 13rem。
+// flex：这个类使元素成为一个 flex 容器，这样其子元素就可以使用 flex 布局。
+// flex-col：这个类使 flex 容器的子元素沿着列方向排列。
+// justify-center 和 items-center：这些类使 flex 容器的子元素在主轴和交叉轴上居中对齐。
+// bg-white：这个类设置元素的背景颜色为白色。
+// pt-6 和 pb-4：这些类分别设置元素的上内边距和下内边距。
+// card-shadow：这个类可能是一个自定义类，用于给元素添加阴影效果。
+// ml-2：这个类设置元素的左外边距。
+// dark:bg-dark 和 dark:card-shadow-dark：这些类在 dark 模式下设置元素的背景颜色和阴影效果。
+// px-10：这个类设置元素的左右内边距。
+// rounded-full：这个类给元素添加最大的圆角，使其成为一个圆形。
+// dark:filter-dark：这个类在 dark 模式下给元素添加一个滤镜效果。
+// mt-2：这个类设置元素的上外边距。
+// font-semibold：这个类设置元素的字体粗细为 semi-bold。
+// text-gray-600：这个类设置元素的文本颜色为灰色，亮度为 600。
+// mb-2：这个类设置元素的下外边距。
+// text-sm：这个类设置元素的文本大小为小号。
+// text-gray-500：这个类设置元素的文本颜色为灰色，亮度为 500。
+// group：这个类用于创建一个 hover、focus、active 状态的分组，使得你可以在一个元素上触发状态改变时，影响其它元素的样式。
+// px-1：这个类设置元素的左右内边距。
+// group-hover:text-gray-900：这个类在元素的父元素（带有 group 类的元素）被 hover 时，设置元素的文本颜色为灰色，亮度为 900。
+// font-bold：这个类设置元素的字体粗细为 bold。
+// group-hover:font-black：这个类在元素的父元素（带有 group 类的元素）被 hover 时，设置元素的字体粗细为最粗。
+// group-hover:font-normal：这个类在元素的父元素（带有 group 类的元素）被 hover 时，设置元素的字体粗细为 normal。
+// mt-4：这个类设置元素的上外边距。
+// w-full：这个类设置元素的宽度为 100%。
+// justify-center：这个类使 flex 容器的子元素在主轴上居中对齐。
+// px-2 和 py-1：这些类分别设置元素的左右内边距和上下内边距。
+// select-none：这个类禁止用户选中元素的文本。
+// cursor-pointer：这个类使元素的光标在 hover 时变为手形。
+// hover:bg-gray-200：这个类在元素被 hover 时，设置元素的背景颜色为灰色，亮度为 200。
+// rounded-sm：这个类给元素添加一个小的圆角。
+// transition-all：这个类使元素的所有属性在改变时都有过渡效果。
+// text-xs：这个类设置元素的文本大小为最小号。
 export default function (props: { option: AuthorCardProps }) {
   const { theme } = useContext(ThemeContext);
 
